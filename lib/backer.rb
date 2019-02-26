a98cdde1582e7edfcb0e2925e381861fa3ad4a42
Patrick Kim <pkim050@gmail.com>
2019-02-26 21:32:45 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Backer
+  attr_accessor :name
+  attr_reader :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(class_project)
+    @backed_projects << class_project
+    binding.pry
+  end
+  
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+  attr_accessor
+  attr_reader :backers, :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(class_backer)
+    @backers << class_backer
+  end
+end
